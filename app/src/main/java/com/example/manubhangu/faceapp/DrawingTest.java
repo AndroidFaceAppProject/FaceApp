9ffb0e43e0b4833129f612207f1bdbba40800a24
Norman Lim <Norman.Lim2@hotmail.com>
2016-11-18 08:50:38 -0800

    Added paint page drawing functionality. Paintbrush colors available.
    Signed-off-by: Norman Lim <Norman.Lim2@hotmail.com>

--- app/src/main/java/com/example/manubhangu/faceapp/DrawingTest.java
+++ app/src/main/java/com/example/manubhangu/faceapp/DrawingTest.java
@@ -3,12 +3,48 @@ package com.example.manubhangu.faceapp;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class DrawingTest extends Activity {
+
+    private DrawingView drawView;
+
+    private ImageButton currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing_test);
+
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        //retrieve the Linear Layout it is contained within
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+
+        // Get the first button and store it as the instance variable
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+
+        //user has clicked a paint color that is not the currently selected one
+        if(view!=currPaint){
+            //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+
+            drawView.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+
+
     }
 }
